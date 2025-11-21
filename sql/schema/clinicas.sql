dab72958e02392bea5f75bb852e24e72a0e46d21
David Desktop <davidaguasoliveira@gmail.com>
2025-11-21 21:52:49 +0000

    Changes on sql

--- sql/schema/clinicas.sql
+++ sql/schema/clinicas.sql
@@ -1,6 +1,8 @@
 CREATE TABLE clinicas (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL, 
     cp VARCHAR(8) NOT NULL,
     nif VARCHAR(9),
     iban varchar(25) NOT NULL
--- sql/schema/veterinarios.sql
+++ sql/schema/veterinarios.sql
@@ -1,9 +1,9 @@
-CREATE TABLE veterinarios (
+CREATE TABLE funcionarios(
     id SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     especialidade VARCHAR(100),
     telefone VARCHAR(20) NOT NULL,
-    email VARCHAR(100),
+    email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     id_clinica INT NOT NULL REFERENCES clinicas(id) ON DELETE CASCADE
 );
