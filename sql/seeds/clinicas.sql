f06b2323d9323572f6c468d3e061181747ebf99f
David Oliveira <david@pop-os.home>
2025-11-21 21:00:59 +0000

    SQL seeds added and to_do list

---
+++ sql/seeds/clinicas.sql
@@ -0,0 +1,5 @@
+-- Inserir dados de exemplo na tabela clinicas
+INSERT INTO clinicas (nome, cp, nif) VALUES
+('Clinica Veterinaria Central', '4000-001', '123456789'),
+('Clinica Animal Porto', '4050-123', '987654321'),
+('Clinica PetCare Gaia', '4430-567', '456123789');
