d415ef4ab7514fff3eaa26e6d17bdd042db5c953
David Desktop <davidaguasoliveira@gmail.com>
2025-12-13 23:44:01 +0000

    First commit api

--- src/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ src/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d935405e524aacadd7a674c6cc88eb73a56c78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c48f5176f3843060772c9a5495c5ad98e962fe4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
