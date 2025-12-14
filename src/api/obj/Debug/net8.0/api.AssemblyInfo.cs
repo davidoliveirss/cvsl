39669ae864d30a0e28bdd726ef8540b50d1e99f9
David Desktop <davidaguasoliveira@gmail.com>
2025-12-14 01:04:14 +0000

    Added table categorias and changed table produtos

--- src/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ src/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c48f5176f3843060772c9a5495c5ad98e962fe4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c797efd0521a29b9d2fb952b0550e3d87060165")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
