96a5b0ecd0945f7ee4a5c6d544f8ecbff379978e
David Desktop <davidaguasoliveira@gmail.com>
2025-12-14 00:25:40 +0000

    Added comments on the swagger

--- src/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ src/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c48f5176f3843060772c9a5495c5ad98e962fe4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6950ae66d0684dc57706f082cfc296d17e337282")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
