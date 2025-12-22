9568046a56d7c4421329ba22798d5c7ef203ade9
david <davidaguasoliveira@gmail.com>
2025-12-22 15:54:06 +0000

    Added status on product, the product now its now deleted only deativated

--- sql/schema/produtos.sql
+++ sql/schema/produtos.sql
@@ -5,5 +5,6 @@ CREATE TABLE produtos (
     preco DECIMAL(10,2) NOT NULL,
     unidades_por_caixa INT NOT NULL,
     quantidade_stock INT NOT NULL DEFAULT 0,
-    id_clinica INT NOT NULL REFERENCES clinicas(id) ON DELETE CASCADE
+    id_clinica INT NOT NULL REFERENCES clinicas(id) ON DELETE CASCADE,
+    ativo BOOLEAN DEFAULT TRUE
 );
